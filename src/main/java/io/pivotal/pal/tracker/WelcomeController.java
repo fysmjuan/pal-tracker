8174c89c17a0b8f6e818de2d79fb4bbd9fe3fa4e
ME <fysmjuan@yahoo.com>
2019-12-02 13:41:56 -0700

    work 2

--- src/main/java/io/pivotal/pal/tracker/WelcomeController.java
+++ src/main/java/io/pivotal/pal/tracker/WelcomeController.java
@@ -1,13 +1,21 @@
 package io.pivotal.pal.tracker;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class WelcomeController {
 
+    String message;
+
+    public WelcomeController (@Value("${welcome.message:Default hello}") final String msg) {
+        message = msg;
+    }
+
+
     @GetMapping("/")
     public String sayHello() {
-        return "hello";
+        return message;
     }
 }
